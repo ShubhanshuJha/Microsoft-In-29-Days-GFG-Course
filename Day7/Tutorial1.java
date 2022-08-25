0c4dea418fe1b2fd382e3e5c966ad12dccbc19ac
Shubhanshu Jha <shubhanshujha.s19@gmail.com>
2022-08-25 23:28:00 +0530

    Distribute N candies among K people

---
+++ Day7/Tutorial1.java
@@ -0,0 +1,18 @@
+/* Distribute N candies among K people
+Given N candies and K people. In the first turn, the first person gets 1 candy, the second gets 2 candies, and so on
+till K people. In the next turn, the first person gets K+1 candies, the second person gets K+2 candies and so on.
+If the number of candies is less than the required number of candies at every turn, then the person receives the
+remaining number of candies. Find the total number of candies every person has at the end.
+
+Example 1:	Input:	N = 7, K = 4
+Output:	1 2 3 1
+Explanation: At the first turn, the fourth person has to be given 4 candies, but there is only 1 left,
+		hence he takes only one. 
+
+Example 2:	Input:	N = 10, K = 3
+Output:	5 2 3
+Explanation: At the second turn first one receives 4 and then we have no more candies left. */
+
+
+
+
