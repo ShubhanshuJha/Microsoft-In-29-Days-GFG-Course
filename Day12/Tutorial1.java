8458c724cb97f0e5a6a56dacc70cfaf96c76f8ea
Shubhanshu Jha <shubhanshujha.s19@gmail.com>
2022-09-23 14:20:37 +0530

    Word Boggle

--- Day12/Tutorial1.java
+++ Day12/Tutorial1.java
@@ -19,53 +19,53 @@ Explanation:    G I Z
                 Q S E
 Words we got is denoted using same color. */
 
+// DFS Approach
 class Solution {
-    public String[] wordBoggle(char[][] board, String[] dictionary) {
-        ArrayList<String> lst = new ArrayList<>();
+    public String[] wordBoggle(char[][] board, String[] dictionary){
         int m = board.length, n = board[0].length;
-        
+        ArrayList<String> lst = new ArrayList<>();
         for (String word : dictionary) {
-            if (search(board, word, m, n)) lst.add(word);
+            if (search(word, board, m, n)) lst.add(word);
         }
         String[] res = new String[lst.size()];
         for (int i = 0; i < res.length; i++) res[i] = lst.get(i);
         return res;
     }
-    
-    private boolean search(char[][] board, String word, int r, int c) {
+    private boolean search(String word, char[][] grid, int r, int c) {
         boolean[][] vis = new boolean[r][c];
+        
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    if (dfs(board, word, vis, i, j, r, c, 0)) {
-                        return true;
-                    }
-                }
+                if (grid[i][j] == word.charAt(
+                    0) && dfs(word, grid, i, j, r, c, 0, vis))
+                    return true;
             }
         }
         return false;
     }
-    // All possible directions co-ordinates
-    private int[] dx = {1, 1, 0, -1, -1, -1, 0, 1};
-    private int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+    // poss. dir.: ULdiag, up, URdiag, left, right, LLdiag, down, LRdiag
+    private final int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+    private final int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
     
-    private boolean dfs(char[][] board, String word, boolean[][] vis, int x, int y, int r, int c, int idx) {
-        if (x < 0 || x >= r || y < 0 || y >= c || vis[x][y] || word.charAt(idx) != board[x][y]
-            || idx >= word.length()) return false;
-        
-        if (idx == word.length() - 1) return true;
+    private boolean dfs(String word, char[][] grid, int x, int y, int r, int c, int idx, boolean[][] vis) {
+        // if idx out of bound or vis[x][y] or char not matching return false
+        if (x < 0 || x >= r || y < 0 || y >= c || idx >= word.length() || vis[x][y] || word.charAt(idx) != grid[x][y])
+            return false;
+        if (idx == word.length() - 1)
+            return true;
         
         vis[x][y] = true;
-        for (int i = 0; i < 8; i++) {
+        
+        for (int i = 0; i < 8; i++) { // checking for each direct.
             int pX = x + dx[i];
             int pY = y + dy[i];
-            if (dfs(board, word, vis, pX, pY, r, c, idx + 1)) {
-                return true;
-            }
+            if (dfs(word, grid, pX, pY, r, c, idx + 1, vis))
+                return true; // word found
         }
-        // if not found, then backtrack
+        // if word not found, then backtrack
         vis[x][y] = false;
         return false;
     }
 }
+
 
