aee28fda134c1af01473235ec2624db809c9d2bf
Shubhanshu Jha <shubhanshujha.s19@gmail.com>
2022-09-29 15:26:31 +0530

    Shortest Unique prefix for every word

--- Day13/Tutorial1.java
+++ Day13/Tutorial1.java
@@ -14,7 +14,9 @@ Explanation:	geeksgeeks => geeksg
 				geeksquiz => geeksq 
 				geeksforgeeks => geeksf  */
 
+// Trie Approach
 class Solution {
+    // Time: O(N*length of each word)      Space: O(N*length of each word)
     static String[] findPrefixes(String[] arr, int N) {
         Trie root = new Trie();
         for (String str : arr) {
@@ -31,10 +33,10 @@ class Solution {
 class Node {
     final int SIZE = 26;
     Node[] children;
-    int[] count;
+    int count;
     Node() {
         this.children = new Node[SIZE];
-        this.count = new int[SIZE];
+        this.count = 0;
     }
 }
 
@@ -46,20 +48,20 @@ class Trie {
     public void insert(String key) {
         Node curr = this.root;
         for (char ch : key.toCharArray()) {
-            curr.count[ch - 'a']++;
             if (curr.children[ch - 'a'] == null) {
                 curr.children[ch - 'a'] = new Node();
             }
             curr = curr.children[ch - 'a'];
+            curr.count++;
         }
     }
     public String search(String key) {
         Node curr = this.root;
         String res = "";
         for (char ch : key.toCharArray()) {
-            res += ch;
-            if (curr.count[ch - 'a'] == 1) break;
             curr = curr.children[ch - 'a'];
+            res += ch;
+            if (curr.count == 1) break;
         }
         return res;
     }
