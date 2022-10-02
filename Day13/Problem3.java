e8d9d62827d9d7e3b906da57bd84464c70c4366d
Shubhanshu Jha <shubhanshujha.s19@gmail.com>
2022-10-02 23:39:17 +0530

    Palindrome Pairs

---
+++ Day13/Problem3.java
@@ -0,0 +1,71 @@
+/* Palindrome Pairs
+Given an array of strings arr[] of size N, find if there exists 2 strings arr[i] and arr[j] such that arr[i]+arr[j] is a palindrome i.e the
+concatenation of string arr[i] and arr[j] results into a palindrome.
+
+Example 1:	Input:	N = 6,	arr[] = {"geekf", "geeks", "or","keeg", "abc", "bc"}
+Output:	1
+Explanation: There is a pair "geekf" and "keeg".
+
+Example 2:	Input:	N = 5,	arr[] = {"abc", "xyxcba", "geekst", "or", "bc"}
+Output:	1
+Explanation: There is a pair "abc" and "xyxcba".  */
+
+
+// HashMap  Approach
+class Solution {
+    private static boolean isPalin(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i <= j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    // Time: O(N * (l ^ 2))		Space: O(N * (l ^ 2)), 	where l = length of longest string
+    public static int palindromepair(int N, String[] arr) {
+        if (N == 1) return 0;
+        HashMap<String, Integer> hmap = new HashMap<>();
+        for (int i = 0; i < N; i++)
+            hmap.put(arr[i], i);
+
+        StringBuilder left = null, right = null;
+        for (int i = 0; i < N; i++) {
+            if (arr[i].equals("") || arr[i].length() == 0)
+                continue;
+            String temp = reverse(arr[i]);
+            if (hmap.containsKey(temp) && hmap.get(temp) != i)
+                return 1;
+            int l = arr[i].length();
+            left = new StringBuilder();
+            for (int j = 0; j < l - 1; j++) {
+                left.append(arr[i].charAt(j));
+                right = new StringBuilder(arr[i].substring(j + 1, l));
+                if (isPalin(left.toString())) {
+                    String rev = reverse(right.toString());;
+                    if (hmap.containsKey(rev) && hmap.get(rev) != i)
+                        return 1;
+                }
+                if (isPalin(right.toString())) {
+                    String rev = reverse(left.toString());
+                    if (hmap.containsKey(rev) && hmap.get(rev) != i)
+                        return 1;
+                }
+            }
+        }
+        return 0;
+    }
+    private static String reverse(String str) {
+        StringBuffer sb = new StringBuffer();
+        int i = str.length() - 1;
+        while (i >= 0) {
+            sb.append(str.charAt(i));
+            i--;
+        }
+        return sb.toString();
+    }
+}
+-----------------------------------------------------------------------------------------------
+
+
