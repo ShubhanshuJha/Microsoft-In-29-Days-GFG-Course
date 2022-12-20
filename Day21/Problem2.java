33c3245b7b839f171421247f4d667713b4e1dcfd
Shubhanshu Jha <shubhanshujha.s19@gmail.com>
2022-12-21 00:54:19 +0530

    Distance of nearest cell having 1

--- Day21/Problem2.java
+++ Day21/Problem2.java
@@ -23,48 +23,46 @@ Explanation: The grid is-
 	0's at (0,1), (1,2), (2,1) and (2,2) are at a distance of 1, 1, 1 and 2 from 1's at (0,0), (0,2), (2,0) and (1,1) respectively. */
 
 
+// BFS Approach
 class Solution {
     private int[] dx = {-1, 1, 0, 0};
     private int[] dy = {0, 0, -1, 1};
-    // Time: O(nm)		Space: O(nm)
+    // Time: O(m*n)      Space: O(m*n)
     public int[][] nearest(int[][] grid) {
-        int n = grid.length, m = grid[0].length;
-        int[][] res = new int[n][m];
+        int m = grid.length, n = grid[0].length;
+        int[][] res = new int[m][n];
+        Queue<int[]> q = new LinkedList<>();
         
-        for (int i = 0; i < n; i++)
-            Arrays.fill(res[i], Integer.MAX_VALUE);
-        
-        boolean[][] vis = new boolean[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 0 && !vis[i][j]) {
-                    calc(grid, res, n, m, vis, i, j, i, j);
-                } else if (grid[i][j] == 1) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // initializing the res[][] with -1
+                res[i][j] = -1;
+                
+                if (grid[i][j] == 1) {
                     res[i][j] = 0;
+                    q.offer(new int[]{i, j});
                 }
             }
         }
-        return res;
-    }
-    private int calc(int[][] grid, int[][] res, int n, int m, boolean[][] vis, int r, int c, int r_, int c_) {
-        if (r < 0 || r >= n || c < 0 || c >= m || vis[r][c])
-            return -1;
-        if (grid[r][c] == 1)
-            return Math.abs(r - r_) + Math.abs(c - c_);
         
-        int ans = Integer.MAX_VALUE;
-        
-        for (int d = 0; d < 4; d++) {
-            int x = r + dx[d];
-            int y = c + dy[d];
-            int dist = calc(grid, res, n, m, vis, x, y, r, c);
-            if (dist != -1 && ans > dist) {
-                res[x][y] = Integer.min(res[x][y], dist);
-                ans = dist;
-                vis[x][y] = true;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int r = q.peek()[0],
+                    c = q.peek()[1];
+                q.poll();
+                
+                for (int d = 0; d < 4; d++) {
+                    int x = r + dx[d];
+                    int y = c + dy[d];
+                    
+                    if (x < 0 || x >= m || y < 0 || y >= n || res[x][y] != -1) continue;
+                    q.offer(new int[]{x, y});
+                    res[x][y] = res[r][c] + 1;
+                }
             }
         }
-        vis[r][c] = true;
-        return res[r][c] = Integer.min(res[r][c], ans);
+        
+        return res;
     }
 }
